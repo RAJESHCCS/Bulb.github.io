58958d563d5424e043cb933cc5c623bb26d86fe6
RAJESH <rajccs1562001@gmail.com>
2022-03-17 11:22:33 +0530

    initial commited
    bulb project

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function turnOn() {
+    var img = document.getElementById('image');
+    img.src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function turnOff() {
+    var img = document.getElementById('image');
+    img.src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
